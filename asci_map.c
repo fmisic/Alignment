38766438ce9f203ff1a9acdc97ed7fd22c6a98ec
fmisic <80854855+fmisic@users.noreply.github.com>
2023-10-08 19:27:02 +0200

    Add files via upload

---
+++ asci_map.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "asci_map.h"
+
+void set_asci_map() {
+	ASCI_MAP[0] = 0;
+    ASCI_MAP[1] = -1;
+    ASCI_MAP[2] = 4;
+    ASCI_MAP[3] = 3;
+    ASCI_MAP[4] = 6;
+    ASCI_MAP[5] = 13;
+    ASCI_MAP[6] = 7;
+    ASCI_MAP[7] = 8;
+    ASCI_MAP[8] = 9;
+    ASCI_MAP[9] = -1;
+    ASCI_MAP[10] = 11;
+    ASCI_MAP[11] = 10;
+    ASCI_MAP[12] = 12;
+    ASCI_MAP[13] = 2;
+    ASCI_MAP[14] = -1;
+    ASCI_MAP[15] = 14;
+    ASCI_MAP[16] = 5;
+    ASCI_MAP[17] = 1;
+    ASCI_MAP[18] = 15;
+    ASCI_MAP[19] = 16;
+    ASCI_MAP[20] = -1;
+    ASCI_MAP[21] = 19;
+    ASCI_MAP[22] = 17;
+    ASCI_MAP[23] = -1;
+    ASCI_MAP[24] = 18;
+    ASCI_MAP[25] = -1;
+}
+
+
+
+
